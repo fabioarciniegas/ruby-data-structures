e21f7ccecec1ce055c9d1b4bcfdda956ee27ab9a
Fabio Arciniegas <fab.arciniegas@gmail.com>
2013-05-06 12:40:16 -0700

    spec

---
+++ spec/priority_queue_spec.rb
@@ -0,0 +1,71 @@
+require_relative '../PriorityQueue'
+
+describe PriorityQueue do
+  before :each do
+    @heap = PriorityQueue.new(1)
+  end
+
+  describe "#new" do
+    it "takes zero or one parameter with the node value" do
+      @heap.should be_an_instance_of PriorityQueue
+    end
+  end
+
+  describe ".insert" do
+    it "should do insert and still be a heap" do
+      @heap.insert(2)
+      @heap.insert(3)
+      @heap.weight.should eql 3
+      @heap = PriorityQueue.new()
+      @heap.insert(2)
+      @heap.insert(3)
+      @heap.weight.should eql 2
+      @heap.isHeap.should eql true 
+      (0...1001).each do |n| @heap << n end
+      @heap.isHeap.should eql true 
+    end
+  end
+
+  describe ".pop!" do
+    it "should do pop" do
+      @heap.insert(2)
+      @heap.insert(3)
+      @heap.weight.should eql 3
+      @heap.pop!.should eql 3
+      @heap.pop!.should eql 2
+      @heap.pop!.should eql 1
+      @heap.weight.should eql 0
+    end
+  end
+
+  describe "roundtrip with many elements" do
+    it "should return items in order" do
+      a = Array.new
+      b = Array.new
+      h = PriorityQueue.new
+      (0...100).each do |n| a << n end
+      (0...100).each do |n| b << n end
+      a.shuffle
+      h.isHeap.should eql true 
+      a.each do |n| h << n end
+      b.reverse_each do |n| 
+        h.pop!.should eql n 
+        h.isHeap.should eql true 
+      end
+      h.pop!.should eql nil
+      h.weight.should eql 0
+    end
+  end
+
+  describe "empty is not nil" do
+    it "should be non-nil even if empty" do
+      h = PriorityQueue.new
+      h << 3
+      a = h.pop!
+      a.should eql 3
+      h.nil?.should eql false
+      h.empty?.should eql true
+    end
+  end
+
+end
